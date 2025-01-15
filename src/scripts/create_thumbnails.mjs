c4e0d9bd432a6ce5a61a1cf6b52049d01b232855
Nikolaos Vitsas <vineek@Mac.home>
2025-01-15 13:50:19 +0200

    test run for thumbnail creation

--- src/scripts/create_thumbnails.mjs
+++ src/scripts/create_thumbnails.mjs
@@ -1,7 +1,47 @@
 import fs from 'fs';
 import path from 'path';
 import { Worker } from 'worker_threads';
-import sharp from 'sharp';
+
+async function* getFiles(dir) {
+  const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    //const res = path.resolve(dir, dirent.name);
+    const res = path.join(dir, dirent.name);
+    
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+const image_directory = 'public/images';
+const thumbnail_directory = 'public/thumbnails';
+//const image_directory = '/Users/vineek/Documents/Projects/Web/glTF-Render-Fidelity/test/goldens';
+
+fs.rmSync(thumbnail_directory, { recursive: true, force: true });
+
+const tasks = [];
+await (async () => {
+  for await (const f of getFiles(image_directory)) {
+    const src_directory = path.dirname(f);
+    const src_relative_file = path.relative(image_directory, f);
+    const tgt_f = thumbnail_directory + '/' + src_relative_file;
+    const tgt_directory = path.dirname(tgt_f);
+    const relative_directory = path.relative(thumbnail_directory, src_relative_file);
+    const filename = path.basename(src_relative_file, path.extname(src_relative_file));
+    const ext = path.extname(f);
+    tasks.push({
+      src: f,
+      tgt: tgt_directory + '/' + filename + '.thumb' + '.webp',
+      tgt_dir: tgt_directory,
+      rel: src_relative_file,
+      ext: ext,
+      name: filename
+    });
+  }
+})();
 
 // Function to run a task in a worker
 function runWorker(filePath, workerData) {
@@ -20,71 +60,12 @@ function runWorker(filePath, workerData) {
 
 // Main function to start multiple workers
 async function runTasks() {
-  const tasks = [1, 2, 3, 4, 5]; // Example tasks
   const workers = tasks.map((task) =>
     runWorker('./src/scripts/thumbnail_worker.mjs', task)
   );
 
   const results = await Promise.all(workers);
-  console.log('All results:', results);
+  return results;
 }
 
-runTasks().catch((err) => console.error(err));
-
-/*await sharp('./public/original.jpeg')
-  .resize(4)
-  .jpeg({ mozjpeg: true })
-  //.toBuffer()
-  .toFile('./public/thumbnails/image.jpeg', (err, info) => {
-    if (err) {
-      console.log('Error processing image:', err);
-    } else {
-      console.log('Image successfully saved:', info);
-    }
-  });*/
-
-  try {
-    fs.mkdirSync('./public/thumbnails2', { recursive: true });
-    console.log('Directory created successfully!');
-  } catch (err) {
-    console.error('Error creating directory:', err);
-  }
-
-/*
-try {
-  fs.mkdirSync('./public/thumbnails', { recursive: true });
-  console.log('Directory created successfully!');
-} catch (err) {
-  console.error('Error creating directory:', err);
-}
-
-try {
-  const directoryPath = './public/images';
-  const files = fs.readdirSync(directoryPath);
-  console.log('Files in the directory:', files);
-  files.forEach(file => {
-    const filePath = path.join(directoryPath, file);  // Get the full path of each item
-    const stats = fs.statSync(filePath);  // Get file or directory stats
-
-    if (stats.isDirectory()) {
-      console.log(`${file} is a directory.`);
-    } else if (stats.isFile()) {
-      console.log(`${file} is a file.`);
-    }
-  });
-} catch (err) {
-  console.error('Error reading the directory:', err);
-}*/
-/*
-await sharp('./public/original.jpeg')
-  .resize(4)
-  .jpeg({ mozjpeg: true })
-  //.toBuffer()
-  .toFile('./public/thumbnails/image.jpeg', (err, info) => {
-    if (err) {
-      console.log('Error processing image:', err);
-    } else {
-      console.log('Image successfully saved:', info);
-    }
-  });
- */
+const results = await runTasks().catch((err) => console.error(err));
--- src/scripts/thumbnail_worker.mjs
+++ src/scripts/thumbnail_worker.mjs
@@ -1,5 +1,27 @@
 import { parentPort, workerData } from 'worker_threads';
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
 
 // Simulate a time-consuming task
-const result = workerData * 2; // Example task: double the input
+const result = workerData; // Example task: double the input
+
+try {
+  fs.mkdirSync(workerData.tgt_dir, { recursive: true });
+  console.log(`Directory ${workerData.tgt_dir} created successfully!`);
+} catch (err) {
+  console.error('Error creating directory:', err);
+}
+
+await sharp(workerData.src)
+  .resize(64)
+  .webp({quality: 75})
+  .toFile(workerData.tgt, (err, info) => {
+    if (err) {
+      console.log('Error processing image:', workerData.tgt);
+    } else {
+      console.log('Image successfully saved:', workerData.tgt);
+    }
+});
+
 parentPort.postMessage(result); // Send the result back to the main thread
