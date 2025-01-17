28c6a3875ea758a72ab06053e08945c1a94377e4
agkar <a.gkaravelis@gmail.com>
2025-01-17 14:23:10 +0200

    update routes

--- src/app/model/[name]/page.tsx
+++ src/app/model/[name]/page.tsx
@@ -1,4 +1,16 @@
-export default async function Page({params }: { params: Promise<{ name: string }> }) {
-    const name = (await params).name
-  return <h1>Hello, {name}!</h1>
+import models from "@/data/models.json"
+
+export const dynamicParams = false; // models that are not included in the list, generate 404
+
+export async function generateStaticParams() {
+    
+    return models.models.map((model) => ({
+      name: model.name,
+      description: model.description
+    }))
+}
+
+export default async function Page({params}: { params: Promise<{ name: string, description: string }> }) {
+    const { name, description } = await params;
+  return <h1>Hello, {name}! with description {description}</h1>
 }
