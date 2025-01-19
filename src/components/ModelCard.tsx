2a5214c4075d76401093b764fb4dac6065455ffb
Vineek <nickvitsas@gmail.com>
2025-01-19 12:19:54 +0200

    Add route to cards

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link';
 import { Box, Chip, Typography, Grid2 as Grid } from "@mui/material";
 
 export type ModelCardProps = {
@@ -11,15 +12,17 @@ export default function ModelCard({name}: ModelCardProps) {
     return (
         <Box sx={{boxShadow:16, width: '300px', borderRadius: '16px', background:'#FEF7FF', margin: '10px'}}>
             <Grid container justifyContent="center">
-              <img
-                style={{textAlign: "center", borderTopLeftRadius: '16px', borderTopRightRadius: '16px'}}
-                width={"100%"}
-                height={"256px"}
-                srcSet={`./thumbnails/original.thumb.webp`}
-                src={`./thumbnails/original.thumb.webp`}
+            <Link href="/model/luda">
+              <Image
+                style={{textAlign: "center", cursor: 'pointer', width:"100%", borderTopLeftRadius: '16px', borderTopRightRadius: '16px'}}
+                height={256}
+                width={256}
+                srcSet={`/thumbnails/original.thumb.webp`}
+                src={`/thumbnails/original.thumb.webp`}
                 alt={name}
                 loading="lazy"
               />
+            </Link>
             </Grid>
             <Grid container justifyContent="left">
               <Typography fontSize={18} fontWeight={'bold'} sx={{m:1}}>{name}</Typography>
