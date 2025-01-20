261e23dd092f542dc2a4812e7d0069805298cf2c
agkar <a.gkaravelis@gmail.com>
2025-01-20 17:42:33 +0200

    small change to fix build errors

--- src/components/ImageComparisonSlider.tsx
+++ src/components/ImageComparisonSlider.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import { Box } from "@mui/material";
 
 export type ImageComparisonSliderProps = {
-    imgA: string,
-    imgB: string
+  imgSrc1: string,
+  imgSrc2: string
 }
 
 const ImageComparison2 = ({imgSrc1, imgSrc2}: ImageComparisonSliderProps) => {
