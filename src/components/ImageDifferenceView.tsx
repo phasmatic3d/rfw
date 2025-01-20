229b639fae11ec319f88b48443e01863d18037da
agkar <a.gkaravelis@gmail.com>
2025-01-20 17:35:45 +0200

    add image difference

---
+++ src/components/ImageDifferenceView.tsx
@@ -0,0 +1,68 @@
+"use client"
+import React from 'react'
+import { Box } from "@mui/material";
+
+export type ImageComparisonSliderProps = {
+  imgSrc1: string,
+  imgSrc2: string
+}
+
+export default function ImageDifferenceView({imgSrc1, imgSrc2}: ImageComparisonSliderProps) {
+
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+
+    if(canvasRef == null || canvasRef.current == null) { return; }
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    const processImages = async () => {
+
+      // Load the images
+      const loadImage = (src: string) =>
+        new Promise((resolve) => {
+          const img = new Image();
+          img.src = src;
+          img.crossOrigin = '';
+          img.onload = () => resolve(img);
+        }
+      );
+      const [img1, img2] = await Promise.all([loadImage(imgSrc1), loadImage(imgSrc2)]) as HTMLImageElement[];
+
+      const width = img1.width;
+      const height = img1.height;
+
+      // Set canvas size to match the images
+      canvas.width = img1.width;
+      canvas.height = img1.height;
+
+      // Draw images onto the canvas
+      context.drawImage(img1, 0, 0);
+      const img1Data = context.getImageData(0, 0, width, height);
+      context.drawImage(img2, 0, 0);
+      const img2Data = context.getImageData(0, 0, width, height);
+
+      const factor = 10;
+
+      // Compute the difference
+      const diffData = context.createImageData(width, height);
+      for (let i = 0; i < img1Data.data.length; i += 4) {
+        diffData.data[i] = Math.min(255, Math.abs(img1Data.data[i] - img2Data.data[i]) * factor); // Red
+        diffData.data[i + 1] = Math.min(255, Math.abs(img1Data.data[i + 1] - img2Data.data[i + 1]) * factor); // Green
+        diffData.data[i + 2] = Math.min(255, Math.abs(img1Data.data[i + 2] - img2Data.data[i + 2]) * factor); // Blue
+        diffData.data[i + 3] = 255; // Alpha
+      }
+
+      // Draw the difference on the canvas
+      context.putImageData(diffData, 0, 0);
+    }
+
+    processImages();
+  }, [imgSrc1, imgSrc2]);
+  
+    return (
+      <canvas ref={canvasRef} width='200px' height='200px'/>
+    );
+};
