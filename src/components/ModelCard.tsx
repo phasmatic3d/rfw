64fbca262d1ca839c6ea97be4cdb53b2a6d0af20
agkar <a.gkaravelis@gmail.com>
2025-01-21 15:52:44 +0200

    update theme

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -3,31 +3,38 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
 import { Box, Chip, Typography, Grid2 as Grid } from "@mui/material";
+import { useTheme } from '@mui/material/styles';
 
 export type ModelCardProps = {
     name: string
 }
 
 export default function ModelCard({name}: ModelCardProps) {
-    return (
-        <Box sx={{boxShadow:16, width: '300px', borderRadius: '16px', background:'#FEF7FF', margin: '10px'}}>
-            <Grid container justifyContent="center">
-            <Link href="/model/helmet1">
-              <Image
-                style={{textAlign: "center", cursor: 'pointer', width:"100%", borderTopLeftRadius: '16px', borderTopRightRadius: '16px'}}
-                height={256}
-                width={256}
-                src={`/thumbnails/original.thumb.webp`}
-                alt={name}
-                loading="lazy"
-              />
-            </Link>
-            </Grid>
-            <Grid container justifyContent="left">
-              <Typography fontSize={18} fontWeight={'bold'} sx={{m:1}}>{name}</Typography>
-            </Grid>
-            <Chip label={name} color="primary" sx={{m:0.5}} />
-            <Chip label={name} color="primary" sx={{m:0.5}} />
-        </Box>
-    );
+  const tags = ["Draco", "KTX", "Quantization", "Anisotropic"];
+  const thumbnail = `/images/other/babylon-golden.png`;
+
+  const theme = useTheme();
+
+  return (
+      <Box sx={{boxShadow:16, width: '300px', borderRadius: '16px', margin: '10px', background: theme.palette.grey[900] }}>
+          <Grid container justifyContent="center">
+          <Link href="/model/helmet1">
+            <Image
+              style={{textAlign: "center", cursor: 'pointer', width:"100%", borderTopLeftRadius: '16px', borderTopRightRadius: '16px'}}
+              height={256}
+              width={256}
+              src={thumbnail}
+              alt={name}
+              loading="lazy"
+            />
+          </Link>
+          </Grid>
+          <Box display="flex" flexDirection="column" p={1} >
+            <Typography fontSize={18} fontWeight={'bold'}>{name}</Typography>
+            <Box sx={{mt:1}}>
+              {tags.map(t=> <Chip key={t} label={t} color="primary" sx={{m:0.5}} />)}
+            </Box>
+          </Box>          
+      </Box>
+  );
 }
--- src/theme.tsx
+++ src/theme.tsx
@@ -3,7 +3,11 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({  
   colorSchemes: { 
-    light: true, 
+    light: {
+      palette: {
+        grey: {900:"#f5f5f5", 800:"#eeeeee", 700:"#e0e0e0", 600:"#bdbdbd", 500:"#9e9e9e", 400:"#757575", 300:"#616161", 200:"#424242", 100:"#212121", 50:"#101010"},
+      }
+    }, 
     dark: {
       palette: {
         text: /* used for texts*/ {
@@ -26,6 +30,7 @@ const theme = createTheme({
         // info: {...}
         // success: {...}
         // grey: {50: .., 100...900:, A100, A200, A400, A700},
+        grey: {50:"#fafafa", 100:"#f5f5f5", 200:"#eeeeee", 300:"#e0e0e0", 400:"#bdbdbd", 500:"#9e9e9e",600:"#757575", 700:"#616161", 800:"#424242", 900:"#212121"},
         // divider: rgba()
         background: { default: "#000000", paper: "#333333"},
         action: { /* used for button states, item lists or tables */
