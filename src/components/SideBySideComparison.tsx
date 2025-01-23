3e93bfda0a5de633835d3b5c0c7a95e300d19a4b
agkar <a.gkaravelis@gmail.com>
2025-01-23 17:22:12 +0200

    add sidebyside

---
+++ src/components/SideBySideComparison.tsx
@@ -0,0 +1,86 @@
+"use client"
+import React from 'react'
+import { Box, Grid2 as Grid } from "@mui/material";
+
+export type Props = {
+  imgSrc1: string,
+  imgSrc2: string
+}
+
+const ImageComparison = ({imgSrc1, imgSrc2}: Props) => {    
+    
+    return (
+      <Box display='flex'
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        width="100%"
+      >
+      <Box flex="1" position="relative" height="100%" p={1}>
+        <img
+            src={imgSrc1}
+            alt="Engine 1"
+            style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+            }}
+            />
+      </Box>
+
+      <Box flex="1" position="relative" height="100%" p={1}>
+        <img
+            src={imgSrc2}
+            alt="Engine 2"
+            style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+            }}
+            />
+      </Box>
+  
+      </Box>
+    );
+  };
+
+
+  const ImageComparison2 = ({imgSrc1, imgSrc2}: Props) => {    
+    
+    return (
+      <Box display='flex'
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        width="100%"
+        height="500px" // Set desired height
+      >
+        
+        {/* Background Image */}
+        <img
+          src={imgSrc1}
+          alt="Background"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+          }}
+        />
+  
+        {/* Foreground Image */}
+        <img
+          src={imgSrc2}
+          alt="Foreground"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+          }}
+        />
+  
+      </Box>
+    );
+  };
+
+
+  export default ImageComparison;
