edbdb3e8e284d8f92abedbaeea29ac06e0c1f61e
agkar <a.gkaravelis@gmail.com>
2025-01-23 16:53:07 +0200

    update compare

--- src/components/ComparePage.tsx
+++ src/components/ComparePage.tsx
@@ -5,6 +5,10 @@ import ModelRenderCard from "@/components/ModelRenderCard"
 import ImageComparisonSlider from "@/components/ImageComparisonSlider";
 import styles from "./ComparePage.module.css";
 import InfoIcon from '@mui/icons-material/Info';
+import CompareIcon from '@mui/icons-material/Compare';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import EngineSelection from './EngineSelection';
 
 const render_views = [
   {name: "three.js"},
@@ -23,41 +27,35 @@ type ComparePageProps = {
 }
 
 export default function ComparePage({name}: ComparePageProps) {  
-  // Step 1: Set up state
-  const [isDescriptionVisible, setDescriptionVisible] = React.useState(true); 
-  const [isSelectionVisible, setSelectionVisible] = React.useState(true); 
-  const [width, setWidth] = React.useState(60); // Default width of 200px
-
-  // Step 2: Toggle function
-  const toggleDescription = () => {
-    //setWidth(100);
-    setDescriptionVisible(!isDescriptionVisible);
-  };
-  const toggleSelection = () => {
-    //setWidth(100);
-    setSelectionVisible(!isSelectionVisible);
-  };
+  
+  const [isMagnified, setMagnified] = React.useState(false);
+  const [engine1, setEngine1] = React.useState('three.js');
+  const [engine2, setEngine2] = React.useState('filament.js');
 
   return (
     <>
-       <Grid container direction="column" className={styles.main}>
-        {isDescriptionVisible && <Grid className={styles.description}>
+       <Grid container direction="column" className={styles.main} spacing={1}>
+        {!isMagnified && <Grid className={styles.description}>
           <Typography variant='h6'>{name}</Typography>
           <Typography>The web component lets you declaratively add a 3D model to a web page, while hosting the model on your own site. The goal of the component is to enable adding 3D models to your website without understanding the underlying technology and platforms. The web component supports responsive design, and use cases like augmented reality on some devices. It includes features for accessibility, rendering quality, and interactivity</Typography>
           <Typography>The web component lets you declaratively add a 3D model to a web page, while hosting the model on your own site. The goal of the component is to enable adding 3D models to your website without understanding the underlying technology and platforms. The web component supports responsive design, and use cases like augmented reality on some devices. It includes features for accessibility, rendering quality, and interactivity</Typography>
         </Grid>}
-        <Grid className={styles.tool} sx={{width: `${width}%`}} container spacing={1}>
+        {/* Main */}
+        <Grid className={styles.tool} container spacing={1}>
           <Box flex={1} sx={{display:'flex', width: "100%", justifyContent: 'space-between'}}>
-            <InfoIcon onClick={toggleDescription} sx={{cursor: "pointer" }} />
-            <Typography>Compaing {name}</Typography>
-            <InfoIcon onClick={toggleSelection}  sx={{cursor: "pointer"}} />
+            {isMagnified && <CloseFullscreenIcon onClick={() => setMagnified(false)} sx={{cursor: "pointer" }} /> }
+            {!isMagnified && <OpenInFullIcon onClick={() => setMagnified(true)} sx={{cursor: "pointer" }} /> }
+            <CompareIcon />
           </Box>
-
-          <ImageComparisonSlider imgSrc1={"/thumbnails/original.thumb.webp"} imgSrc2={"/thumbnails/image.jpeg"}/>                        
+          <ImageComparisonSlider imgSrc1={"/images/other/babylon-golden.png"} imgSrc2={"/images/other/model-viewer-golden.png"}/>                     
+          <Box display='flex' justifyContent='space-between' width='100%'>
+            <Box flex={1}><EngineSelection engineName={engine1} engineList={render_views.map(e=> e.name)} handleChange={(name) => { setEngine1(name) }}/></Box>
+            <Box flex={1} display='flex' justifyContent='flex-end'><EngineSelection engineName={engine2} engineList={render_views.map(e=> e.name)} handleChange={(name) => { setEngine2(name) }}/></Box>
+          </Box>
         </Grid>
-        {isSelectionVisible && <Grid className={styles.side} sx={{overflow: "auto"}} container spacing={2}>
+        {!isMagnified && <Box className={styles.side} display={{xs:'none', sm:'flex'}} flexDirection='column' justifyContent='flex-start' alignItems='center'>
           {render_views.map((e,i) => { return <ModelRenderCard key={e.name} name={e.name}/>})}
-        </Grid>}
+        </Box>}
       </Grid>
     </>
   )
