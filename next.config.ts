337baa08cd6e5af6f962b146c9de146ca0e8920e
agkar <a.gkaravelis@gmail.com>
2025-01-27 10:07:36 +0200

    update test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = true || process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "/rfw" : "", // Replace "project" with your GitHub repository name
+  basePath: isProd ? "/rfw/" : "", // Replace "project" with your GitHub repository name
   assetPrefix: isProd ? "/rfw/" : "",
 };
 
