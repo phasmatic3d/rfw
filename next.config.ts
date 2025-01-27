31fe9a803d5b6c2c3aefdd870174a7892995cdfa
agkar <a.gkaravelis@gmail.com>
2025-01-27 09:44:27 +0200

    submit new next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? "/project" : "", // Replace "project" with your GitHub repository name
+  assetPrefix: isProd ? "/project/" : "",
 };
 
 export default nextConfig;
