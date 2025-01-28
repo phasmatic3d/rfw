a16b4675ffcd164abaebc9509025cba1acae854a
agkar <a.gkaravelis@gmail.com>
2025-01-28 14:47:30 +0200

    test1

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: isProd ? "/rfw/" : "", // Replace "project" with your GitHub repository name
   assetPrefix: isProd ? "/rfw/" : "",
+  async rewrites() {
+    return [
+      {
+        source: '/images',
+        destination: '/rfw/images',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
