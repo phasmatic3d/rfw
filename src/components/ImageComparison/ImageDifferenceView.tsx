8fc905511042451ad9c8fdef3de7b1ea9b2a4cdd
agkar <a.gkaravelis@gmail.com>
2025-02-05 13:41:04 +0200

    bugfix image difference

--- src/components/ImageComparison/ImageDifferenceView.tsx
+++ src/components/ImageComparison/ImageDifferenceView.tsx
@@ -44,6 +44,7 @@ export default function ImageDifferenceView({imgSrc1, imgSrc2}: ImageComparisonS
       }
   
       const resizeObserver = new ResizeObserver(() => {
+        if (canvasContainer.clientWidth == 0 || canvasContainer.clientHeight == 0) return;
         if ( canvas.style.width === `${canvasContainer.clientWidth}px` ) return;
           canvas.width = canvasContainer.clientWidth; // Update the actual width
           canvas.height = canvasContainer.clientWidth; // Update the actual height
