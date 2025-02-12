95dc08ea704a211e78b2f4d6ec920bb6d81f114c
Vineek <nickvitsas@gmail.com>
2025-02-12 11:26:23 +0200

    Make sample viewer the default thumbnail renderer

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -177,7 +177,6 @@ export default function LandingPage({models}: LandingPageProps) {
   const fuse = new Fuse(Object.values(models), options)
   
   const result = (searchValue === "") ? Object.values(models).map(e => {return {item: e}}) : fuse.search(searchValue);
-  const result2 = result;
 
   return (
     <>
@@ -242,9 +241,10 @@ export default function LandingPage({models}: LandingPageProps) {
         <Grid container style={{padding: 0, margin: 0}} spacing={1} sx={{ justifyContent: "center"}}>
         {/*Object.entries(models).filter((e,i) => searchValue.length <= i).map((e,i) => { return <ModelCard key={e.name} name={e.name}/>})*/}
         {/*Object.values(models).map((e,i) => { return <ModelCard key={e.name} name={e.name}/>})*/}
-        {/*result2.map((e,i) => { return <ModelCard key={e.item.name} name={e.item.name} title={e.item.label} thumbnail={e.item.images[0].image} tags={e.item.tags}/>})*/}
-        {result2.map((e,i) => { return <ModelCard key={e.item.name} name={e.item.name} title={e.item.label} thumbnail={e.item.images[0].thumbnail} tags={e.item.tags}/>})}
-        </Grid>                        
+        {/*result.map((e,i) => { return <ModelCard key={e.item.name} name={e.item.name} title={e.item.label} thumbnail={e.item.images[0].image} tags={e.item.tags}/>})*/}
+        {/*result.map((e,i) => { return <ModelCard key={e.item.name} name={e.item.name} title={e.item.label} thumbnail={e.item.images[0].thumbnail} tags={e.item.tags}/>})*/}
+        {result.map((e,i) => { return <ModelCard key={e.item.name} name={e.item.name} title={e.item.label} thumbnail={(e.item.images.find((m) => m.name === "gltf-sample-viewer") || e.item.images[0]).thumbnail} tags={e.item.tags}/>})}
+        </Grid>     
     </>
   );
 }
