76d74e24a63e2ac0b8722f8433317a1c125cece3
Vineek <nickvitsas@gmail.com>
2025-02-17 12:21:22 +0200

    Added draft FAQ, About and Contribute with markdown

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,12 @@
+import fs from 'fs';
+import Showdown from "showdown";
+
 export default function About() {
-    return <div>About Page TBD</div>
+    const string = fs.readFileSync("./src/data/ABOUT.md", 'utf8')
+
+    const converter = new Showdown.Converter({ tables: true });
+    const text      = string;
+    const html      = converter.makeHtml(text);
+    
+    return <div style={{margin: "auto", maxWidth: "1900px"}} dangerouslySetInnerHTML={{ __html: html }} />
 }
