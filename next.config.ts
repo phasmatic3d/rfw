028dcf1e3bc5d38b2310798ba5570e19c7811935
agkar <a.gkaravelis@gmail.com>
2025-02-19 14:23:02 +0200

    test3

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/rfw',
+  //basePath: '/rfw',
 };
 
 export default nextConfig;
